fb7143476e726497c68ab952c600d673e689b6e0
Durrell Jules <durrelljules@Mac-mini.attlocal.net>
2021-08-20 11:24:42 -0400

    created dealer.db3, works as intended in sqlite3

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,18 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable("cars", tbl =>{
+    tbl.increments()//primary key
+    tbl.text("vin", 128).unique().notNullable()
+    tbl.text("make", 128).notNullable()
+    tbl.text("model", 128).notNullable()
+    tbl.decimal("mileage", 128).notNullable()
+    tbl.text("title")
+    tbl.text("transmission")
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars")
+
 };
